6aed5842948a3ee751eac077f8f8eef4a9a900bc
ealicolivier <oliviervo@ealico.com>
2025-03-09 10:15:36 +0100

    [ADD] => init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [url, setUrl] = useState(new String(""));
+  const [isGenerated, setIsGenerated] = useState(false);
+  return <></>;
+}
+
+export default App;
